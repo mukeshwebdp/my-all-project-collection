97911a2529c3a522b975df43da4b8c2e0116f6c0
mukeshwebdp <mukesh.webdp@gmail.com>
2024-02-15 18:15:24 +0530

    mobile responsive

--- script.js
+++ script.js
@@ -108,3 +108,14 @@ const textWriter = ()=>{
     }, 300);
 } 
 textWriter();
+
+// start show sidebar nav
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex';
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none';
+}
